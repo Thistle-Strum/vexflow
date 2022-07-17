a55bc42da75e029796f3d21676fdd805ccdc7c53
geofholbrook <geof.holbrook@gmail.com>
2022-07-17 15:40:07 -0400

    ran automatic formatting

--- src/App.js
+++ src/App.js
@@ -1,81 +1,74 @@
-import "./App.css";
-import { Canned } from "./components/Canned";
-import  VexForKeyboard  from './components/VexForKeyboard';
-import { useState } from "react";
-import _ from "lodash";
-import * as Tone from "tone";
-import { ButtonGrid, PianoKeyboard } from "@musicenviro/ui-elements";
-
+import './App.css';
+import { Canned } from './components/Canned';
+import VexForKeyboard from './components/VexForKeyboard';
+import { useState } from 'react';
+import _ from 'lodash';
+import * as Tone from 'tone';
+import { ButtonGrid, PianoKeyboard } from '@musicenviro/ui-elements';
 
 // setInterval(() => {
 //   console.log(Tone.Transport.position);
 // }, 100);
 
 function App() {
-  const [notes, setNotes] = useState([0, 1, 2, 3, 4, 5, 6, 7]);
-  const [notes2, setNotes2] = useState([0, 1, 2, 3, 4, 5, 6, 7]);
-  const [audioStarted, setAudioStarted] = useState(false);
-  const [keyboardNotes, setKeyboardNotes] = useState([]);
+    const [notes, setNotes] = useState([0, 1, 2, 3, 4, 5, 6, 7]);
+    const [notes2, setNotes2] = useState([0, 1, 2, 3, 4, 5, 6, 7]);
+    const [audioStarted, setAudioStarted] = useState(false);
+    const [keyboardNotes, setKeyboardNotes] = useState([]);
 
-  const play = async () => {
-    if (!audioStarted) {
-      await Tone.start();
-      console.log("context started");
-      setAudioStarted(true);
-    }
-    Tone.Transport.start();
-  };
+    const play = async () => {
+        if (!audioStarted) {
+            await Tone.start();
+            console.log('context started');
+            setAudioStarted(true);
+        }
+        Tone.Transport.start();
+    };
 
-  const stop = () => {
-    Tone.Transport.pause();
-  };
+    const stop = () => {
+        Tone.Transport.pause();
+    };
 
-  const permutate = () => {
-    Tone.Transport.cancel();
-  };
+    const permutate = () => {
+        Tone.Transport.cancel();
+    };
 
- const onNoteDown = (keyDown, velocity, depressedKeys) => {
-  console.log(keyDown);
-  console.log(velocity);
-  console.log(depressedKeys);
-  setKeyboardNotes(depressedKeys)
- }
+    const onNoteDown = (keyDown, velocity, depressedKeys) => {
+        console.log(keyDown);
+        console.log(velocity);
+        console.log(depressedKeys);
+        setKeyboardNotes(depressedKeys);
+    };
 
- const onNoteUp = (keyDown, velocity, depressedKeys) => {
-  console.log(keyDown);
-  console.log(velocity);
-  console.log(depressedKeys);
-  // setKeyboardNotes(depressedKeys)
- }
+    const onNoteUp = (keyDown, velocity, depressedKeys) => {
+        console.log(keyDown);
+        console.log(velocity);
+        console.log(depressedKeys);
+        // setKeyboardNotes(depressedKeys)
+    };
 
-  return (
-    <div>
-      <Canned
-        notes={notes}
-        notes2={notes2}
-      />
-      <button
-        onClick={() => {
-          setNotes(_.shuffle(notes));
-          setNotes2(_.shuffle(notes2));
-          permutate();
-          play();
-        }}
-      >
-        permutate
-      </button>
-      <button onClick={play}>Play</button>
-      <button onClick={stop}>Stop</button>
-      <VexForKeyboard keyboardNotes={keyboardNotes} />
-      <div className="keyboard-container">
-        <PianoKeyboard 
-          onNoteDown={onNoteDown}
-          onNoteUp={onNoteUp}
-          />
-      </div>
-      <ButtonGrid />
-    </div>
-  );
+    return (
+        <div>
+            <Canned notes={notes} notes2={notes2} />
+            <button
+                onClick={() => {
+                    setNotes(_.shuffle(notes));
+                    setNotes2(_.shuffle(notes2));
+                    permutate();
+                    play();
+                }}
+            >
+                permutate
+            </button>
+            <button onClick={play}>Play</button>
+            <button onClick={stop}>Stop</button>
+            <VexForKeyboard keyboardNotes={keyboardNotes} />
+            <div className="keyboard-container">
+                <PianoKeyboard onNoteDown={onNoteDown} onNoteUp={onNoteUp} />
+            </div>
+            <ButtonGrid />
+        </div>
+    );
 }
 
 export default App;
